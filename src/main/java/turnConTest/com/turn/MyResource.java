ff25474cdb34f60cdba0169e4589eaf4faefd07a
turnmaus001 <50174184+turnmaus001@users.noreply.github.com>
2020-03-21 16:57:14 +0700

    a
    a

--- src/main/java/turnConTest/com/turn/MyResource.java
+++ src/main/java/turnConTest/com/turn/MyResource.java
@@ -385,7 +385,8 @@ public class MyResource {
 			rs = stmt.executeQuery("SELECT name, value from turnSetting");
 			String name = "";
 			while (rs.next()) {
-				name = rs.getString("name");
+				name = rs.getString("name").trim();
+				System.out.println(name);
 				if (name.equals("admin")) {
 					seting.setPass(rs.getString("value"));
 				} else if ((name.equals("role"))) {
@@ -404,6 +405,7 @@ public class MyResource {
 					seting.setWeeken(rs.getString("value"));
 				} else if ((name.equals("covert"))) {
 					seting.setCovert(rs.getString("value"));
+					System.out.println(seting.getCovert());
 				} else if ((name.equals("cvType"))) {
 					seting.setCovertType(rs.getString("value"));
 				} else if ((name.equals("refresh"))) {
